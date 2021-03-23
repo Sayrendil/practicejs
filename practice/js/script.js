de5644442f8cf379b8702999f5fc82b27b3366f4
Daniyar <daniyarsigaev@mail.ru>
2021-03-23 15:39:17 +0600

    First Programm for JS

--- practice/js/script.js
+++ practice/js/script.js
@@ -49,12 +49,40 @@ console.log(typeof(answers)); */
     ------ Интерполяции ------------
 */
 
-const category = 'toys';
+/* const category = 'toys';
 console.log(`https://someurl.com/${category}/5`); // Интренполяция идет только при исплоьзовании обратных ковычек
 
 const user = "Daniyar";
-alert(`Привет, ${user}`);
+alert(`Привет, ${user}`); */
 
 /* 
     ------! Интерполяци !-------
+*/
+
+/* 
+    --------! 1-Приложение для практики -----------
+*/
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+}
+
+const a = prompt('Один из последних просмотренных фильмов?', '');
+const b = prompt('На сколько оцените его?', '');
+const c = prompt('Один из последних просмотренных фильмов?', '');
+const d = prompt('На сколько оцените его?', '');
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+/* 
+    --------! 1-Приложение для практики -----------
 */
